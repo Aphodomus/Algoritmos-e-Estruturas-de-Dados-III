557b59ba81d26e31900aacbb0cead08211333996
Daniel Vitor de Oliveira Santos <daniel.santos@brius.com.br>
2021-11-16 21:43:48 -0300

    Adicionando vote

--- Vote.java
+++ Vote.java
@@ -1,7 +1,13 @@
 public class Vote {
     // Attributes
+    public int IDVote;
+    public int IDUser;
+    public int IDVoted;
+    public boolean vote;
 
     // Special methods
-
-    //
+    public Vote() {
+        
+    }
+    // Functions and methods
 }
